7740caf629205734ab9458d590f5a2fdc86bade7
Vidhi-TEST <test9.vidhi@gmail.com>
2025-01-13 15:28:12 +0530

    GHT-10 (task) Refactor code of todo list and write unit test cases

--- python/todo.py
+++ python/todo.py
@@ -1,5 +1,6 @@
-# Refactored Todo List
+import unittest
 
+# Refactored Todo List
 class TodoList:
     def __init__(self):
         self.tasks = []
@@ -15,10 +16,28 @@ class TodoList:
             print(task)
 
 # Test the TodoList class
-todo_list = TodoList()
-todo_list.add_task("Buy groceries")
-todo_list.add_task("Clean the house")
-todo_list.add_task("Do laundry")
-todo_list.print_tasks()
-todo_list.remove_task("Clean the house")
-todo_list.print_tasks()
+class TodoListTestCase(unittest.TestCase):
+    def setUp(self):
+        self.todo_list = TodoList()
+
+    def test_add_task(self):
+        self.todo_list.add_task("Buy groceries")
+        self.assertEqual(self.todo_list.tasks, ["Buy groceries"])
+
+    def test_remove_task(self):
+        self.todo_list.add_task("Buy groceries")
+        self.todo_list.add_task("Clean the house")
+        self.todo_list.remove_task("Clean the house")
+        self.assertEqual(self.todo_list.tasks, ["Buy groceries"])
+
+    def test_print_tasks(self):
+        self.todo_list.add_task("Buy groceries")
+        self.todo_list.add_task("Clean the house")
+        self.todo_list.add_task("Do laundry")
+        expected_output = "Buy groceries\nClean the house\nDo laundry\n"
+        with captured_output() as (out, err):
+            self.todo_list.print_tasks()
+        self.assertEqual(out.getvalue(), expected_output)
+
+if __name__ == "__main__":
+    unittest.main()
